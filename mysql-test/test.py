7ce274772095125bb6e43b22bb766f980097f152
NIEXIANRUN <79429063+NIEXIANRUN@users.noreply.github.com>
2021-07-20 17:27:06 +0800

    Delete mysql-test directory

--- mysql-test/test.py
+++
@@ -1,7 +0,0 @@
-with open('config.txt') as f:
-    for line in f.readlines():
-        if '#' in line:
-            continue
-        host_type, ip, port, = line.split(',')
-        break
-    print(host_type, ip, port)
